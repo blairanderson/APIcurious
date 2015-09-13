6e85db559edbbf20de9f7f1ed8b74d8784431fe2
Blair Anderson <Blair81@gmail.com>
2015-09-13 12:24:20 -0700

    use state instead of props at top level

--- app/assets/javascripts/components/dashboard.js.jsx
+++ app/assets/javascripts/components/dashboard.js.jsx
@@ -1,23 +1,24 @@
 var Dashboard = React.createClass ({
-  getDefaultProps: function() {
-    return { data: {} }
+  getInitialState: function(){
+    return window.data
   },
+
   render: function() {
     return (
       <div>
         <div className='col-lg-6'>
           <StatsBar key='statsbar'
-                    starredRepos= { this.props.data.starredRepos }
-                    organizations={ this.props.data.organizations }
-                    repositories= { this.props.data.repositories }
-                    commitHistory={ this.props.data.commit_history } />
+                    starredRepos= { this.state.starredRepos }
+                    organizations={ this.state.organizations }
+                    repositories= { this.state.repositories }
+                    commitHistory={ this.state.commit_history } />
         </div>
         <div className='col-lg-6'>
           <UsersBar key='usersbar'
-                    following=  { this.props.data.users.following }
-                    followers=  { this.props.data.users.followers }
-                    feed=       { this.props.data.received_events }
-                    currentUser={ this.props.data.user } />
+                    following=  { this.state.users.following }
+                    followers=  { this.state.users.followers }
+                    feed=       { this.state.received_events }
+                    currentUser={ this.state.user } />
         </div>
       </div>
     );
